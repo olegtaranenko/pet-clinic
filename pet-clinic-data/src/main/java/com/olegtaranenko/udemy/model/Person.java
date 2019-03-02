2fc97530f43bd63b2756ad2f99e3678bb7b1399e
Oleg Taranenko MP <olegtaranenko@gmail.com>
2019-03-03 00:47:09 +0100

    implement id, close #10

--- pet-clinic-data/src/main/java/com/olegtaranenko/udemy/model/Person.java
+++ pet-clinic-data/src/main/java/com/olegtaranenko/udemy/model/Person.java
@@ -1,6 +1,6 @@
 package com.olegtaranenko.udemy.model;
 
-public class Person {
+public class Person extends BaseEntity{
 
     private String firstName;
 
--- pet-clinic-data/src/main/java/com/olegtaranenko/udemy/model/PetType.java
+++ pet-clinic-data/src/main/java/com/olegtaranenko/udemy/model/PetType.java
@@ -1,6 +1,6 @@
 package com.olegtaranenko.udemy.model;
 
-public class PetType {
+public class PetType extends BaseEntity {
 
     private String name;
 
