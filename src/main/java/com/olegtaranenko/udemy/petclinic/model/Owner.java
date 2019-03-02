6b1c8efeec3a5c9e583e91afc57000a3a34dc06a
Oleg Taranenko <olegtaranenko@gmail.com>
2019-03-02 08:45:39 +0100

    first models

---
+++ src/main/java/com/olegtaranenko/udemy/petclinic/model/Owner.java
@@ -0,0 +1,4 @@
+package com.olegtaranenko.udemy.petclinic.model;
+
+public class Owner extends Person {
+}
---
+++ src/main/java/com/olegtaranenko/udemy/petclinic/model/Person.java
@@ -0,0 +1,25 @@
+package com.olegtaranenko.udemy.petclinic.model;
+
+public class Person {
+
+    private String firstName;
+
+    private String lasttName;
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLasttName() {
+        return lasttName;
+    }
+
+    public void setLasttName(String lasttName) {
+        this.lasttName = lasttName;
+    }
+
+}
