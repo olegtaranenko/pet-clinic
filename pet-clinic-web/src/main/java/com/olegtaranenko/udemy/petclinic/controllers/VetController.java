cdae94edfd0baf79a9ef0cd733ac029688dc62f9
Oleg Taranenko MP <olegtaranenko@gmail.com>
2019-03-03 11:54:07 +0100

    close #7 implement Vet List

---
+++ pet-clinic-web/src/main/java/com/olegtaranenko/udemy/petclinic/controllers/VetController.java
@@ -0,0 +1,12 @@
+package com.olegtaranenko.udemy.petclinic.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class VetController {
+    @RequestMapping({"/vets", "/vets/index", "/vets/index.index.html"})
+    public String listVets() {
+        return "vets/index";
+    }
+}
