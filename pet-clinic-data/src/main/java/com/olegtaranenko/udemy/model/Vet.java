4c426bb0b3254e7b61098cbd621f5475368013ce
Oleg Taranenko <olegtaranenko@gmail.com>
2019-03-04 12:37:43 +0100

    devtools experiments

--- pet-clinic-data/src/main/java/com/olegtaranenko/udemy/model/Vet.java
+++ pet-clinic-data/src/main/java/com/olegtaranenko/udemy/model/Vet.java
@@ -1,4 +1,13 @@
 package com.olegtaranenko.udemy.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Setter
+@Getter
+//@NoArgsConstructor
+@AllArgsConstructor
 public class Vet extends Person {
 }
