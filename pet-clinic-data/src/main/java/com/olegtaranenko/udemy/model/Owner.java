c2d0f9fbf1f85c11d1f6b4309fdd563968f58dd7
Taranenko MBP2 <olegtaranenko@gmail.com>
2019-03-05 01:33:57 +0100

    additiona models; close #15

--- pet-clinic-data/src/main/java/com/olegtaranenko/udemy/model/Owner.java
+++ pet-clinic-data/src/main/java/com/olegtaranenko/udemy/model/Owner.java
@@ -1,4 +1,19 @@
 package com.olegtaranenko.udemy.model;
 
+import java.util.Set;
+
 public class Owner extends Person {
+    private Set<Pet> pets;
+
+    public Owner(Set<Pet> pets) {
+        this.pets = pets;
+    }
+
+    public Set<Pet> getPets() {
+        return pets;
+    }
+
+    public void setPets(Set<Pet> pets) {
+        this.pets = pets;
+    }
 }
---
+++ pet-clinic-data/src/main/java/com/olegtaranenko/udemy/model/Visit.java
@@ -0,0 +1,36 @@
+package com.olegtaranenko.udemy.model;
+
+import java.time.LocalDate;
+
+public class Visit extends BaseEntity {
+
+    private LocalDate data;
+
+    private String description;
+
+    private Pet pet;
+
+    public LocalDate getData() {
+        return data;
+    }
+
+    public void setData(LocalDate data) {
+        this.data = data;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Pet getPet() {
+        return pet;
+    }
+
+    public void setPet(Pet pet) {
+        this.pet = pet;
+    }
+}
