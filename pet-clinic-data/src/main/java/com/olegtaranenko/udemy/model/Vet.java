3147a0162c8e70b182b9b3dc55ba64f502afbbc1
Taranenko MBP2 <olegtaranenko@gmail.com>
2019-03-05 22:18:58 +0100

    update vet to use JPA

--- pet-clinic-data/src/main/java/com/olegtaranenko/udemy/model/Vet.java
+++ pet-clinic-data/src/main/java/com/olegtaranenko/udemy/model/Vet.java
@@ -1,19 +1,17 @@
 package com.olegtaranenko.udemy.model;
 
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
-
+import javax.persistence.*;
 import java.util.HashSet;
 import java.util.Set;
 
-@Setter
-@Getter
-@NoArgsConstructor
-@AllArgsConstructor
+@Entity
+@Table(name = "vets")
 public class Vet extends Person {
 
+    @ManyToMany(fetch = FetchType.EAGER)
+    @JoinTable(name = "vet_specialities",
+            joinColumns = @JoinColumn(name = "vet_id"),
+            inverseJoinColumns = @JoinColumn(name = "speciality_id"))
     private Set<Speciality> specialitys = new HashSet<>();
 
     public Set<Speciality> getSpeciality() {
